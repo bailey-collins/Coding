3d5dcd7ce20f0f32d964ce47e1bfcf8ef6548e3f
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-02-06 17:09:16 -0700

    Update sketch.js

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -20,7 +20,7 @@ function draw() {
     // leg code goes here
     //left
     fill('rgb(85, 208, 76)');
-    ellipse(-50, 10, 50, 150);
+    //ellipse(-50, 10, 50, 150);
     //right
     fill('rgb(85, 208, 76)');
     //ellipse(50, 10, 50, 150);
