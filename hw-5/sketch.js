3411ba2d13c6246ec24495b1911df09826a7d0bd
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-02-13 12:21:09 -0700

    Update sketch.js

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,15 +1,37 @@
 function setup() {
+  //create a canvas for my animation to exist on
   createCanvas(600, 600 );
 }
 
+//set variables for later use
 Let centerSquare = 0;
 Let sideOne = 400;
 
+//creating an animation
 function draw() {
   background( 'rgb( 0, 128, 255)');
 
+//creating a sandbox for squares- I'm looking to make some squares which rotate into diamonds in progressively lighter shades of blue
 push();
-  fill('rgb( 51, 153, 255)');
-  square(30, 20, sideOne);
+
+  //first large square
+  push();
+    fill('rgb( 51, 153, 255)');
+    square(0, 0, sideOne);
+  pop();
+
+  //second medium square
+  push();
+    fill('rgb( 102, 178, 255)');
+    square(0, 0, sideOne*0.5);
+  pop();
+
+  //final small square
+  push();
+    fill('rgb( 153, 204, 255)');
+    square(0, 0, sideOne*0.25)
+  pop();
+  
+//end of square sandbox
 pop();
 }
