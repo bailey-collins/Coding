220ee711e5ade97ef5ff3c25ccba0ab6e2bdff57
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-02-13 08:44:59 -0700

    Update sketch.js

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -2,8 +2,13 @@ function setup() {
   createCanvas( windowWidth, 600 );
 }
 
-Let centerSquare = 0
+Let centerSquare = 0;
 
 function draw() {
-  background( 'rgb( 0, 128, 255)')
+  background( 'rgb( 0, 128, 255)');
+
+push();
+  fill('rgb( 51, 153, 255)');
+  square(30, 20, 100);
+pop();
 }
