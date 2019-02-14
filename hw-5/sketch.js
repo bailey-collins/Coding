f54c5ac404e61ce099efb75ada34270fb501fab1
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-02-13 21:54:21 -0700

    Update sketch.js

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -4,9 +4,12 @@ function setup() {
 }
 
 //set variables for later use
-Let centerSquare = 0;
+// make it easier to make the square sized relatively to each other
 Let sideOne = 500;
+// set the rotation rate for the squares
 Let squareRotations = 0
+// make it so that the squares follow the cursor slightly while still rotating
+Let followingSquare = 0
 
 //creating an animation
 function draw() {
@@ -18,25 +21,25 @@ function draw() {
 //creating a sandbox for squares- I'm looking to make some squares which rotate into diamonds in progressively lighter shades of blue
 push();
 
-//move entire grid to center
-translate(300, 300);
-
   //first large square
   push();
     fill('rgb( 51, 153, 255)');
     square(0, 0, sideOne);
+    squareRotations = (mouseY * 0.1) - 20;
   pop();
 
   //second medium square
   push();
     fill('rgb( 102, 178, 255)');
     square(0, 0, sideOne*0.5);
+    squareRotations = (mouseY * 0.2) - 20;
   pop();
 
   //final small square
   push();
     fill('rgb( 153, 204, 255)');
-    square(0, 0, sideOne*0.25)
+    square(0, 0, sideOne*0.25);
+    squareRotations = (mouseY * 0.3) - 20;
   pop();
 
 //end of square sandbox
