97a8682078822a0905460e65e66ac28e4f16ccb0
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-03-05 21:38:10 -0700

    Created hw-8

---
+++ hw-8/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    // create a canvas to draw my animation
+    createCanvas(600, 600);
+}
+
+function draw() {
+    // set the background color
+    background( 'rgb(78, 40, 125)' );
