9d1d3533298378f95ecce3baaaa662ce2661557d
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-03-06 09:44:03 -0700

    Update sketch.js

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,8 +1,19 @@
+Let rows, x_space;
+Let cols, y_space;
+
 function setup() {
     // create a canvas to draw my animation
-    createCanvas(600, 600);
+    createCanvas( windowWidth, 600 );
+    frameRate( 5 );
+
+    x_space = 100;
+    rows = floor( width / x_space );
+    y_space = 40;
+    cols = floor( height / y_space);
 }
 
 function draw() {
     // set the background color
     background( 'rgb(78, 40, 125)' );
+  }
+}
