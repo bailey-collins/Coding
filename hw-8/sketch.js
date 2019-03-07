3a3a4eb72ec27be758a756c162814c0b4c182555
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-03-07 08:21:47 -0700

    Update sketch.js

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,3 +1,4 @@
+// set the variables for later use
 Let rows, x_space;
 Let cols, y_space;
 
@@ -6,6 +7,7 @@ function setup() {
     createCanvas( windowWidth, 600 );
     frameRate( 5 );
 
+    //set values for the variables
     x_space = 100;
     rows = floor( width / x_space );
     y_space = 40;
@@ -21,12 +23,13 @@ function draw() {
     //set a dummy value for x and y
     Let x = 1;
     Let y = 1;
+    //set a while loop so that triangles appear in random locations on the canvas
     while ( x != 0 ){
       x = floor(random() * rows);
       y = floor(random() * cols);
 
         for (Let y_space > 40;) {
-
+    //set a for loop so that the triangles change sizes and colors
           fill( random(50,100), random(175,255), random(125,80));
           triangle(288, 18, 351, 360, 288, 360);
 
