d9c0dd17a74194b2ef19350cd0b7df8f585717a3
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-03-19 14:28:04 -0600

    Update sketch.js

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,8 +1,8 @@
-Let starSides = [];
 Let stars = [];
 
 function setup(){
   createCanvas( windowWidth, 800 );
+  frameRate(2);
 
   //create and cause the stars to move randomly
   for (var i = 0; i < 20; i++) {
@@ -31,6 +31,31 @@ function draw() {
 
 }
 
+//set star locations in an array
+Let starArr = [
+  [0, -9],
+  [7, 9],
+  [-9, -2],
+  [9, -2],
+  [-7, 9]
+];
+let idx = 0;
+
 function drawStars( stars, time ) {
-  
+  translate( width/2, height/2 );
+
+  Let idxPlus = (idx + 1) % starArr.length;
+
+  Let x1 = starArr[idx][0] * 10;
+  Let y1 = starArr[idx][1] * 10;
+  Let x2 = starArr[idxPlus][0] * 10;
+  Let y2 = starArr[idxPlus][1] * 10;
+
+  //draw the sides of the stars
+  strokeWeight(18);
+  stroke(random(255), random(255), random(255), 150);
+  line(x1, y1, x2, y2);
+
+  //increment the idx
+  idx = (idx + 1) % starArr.length;
 }
