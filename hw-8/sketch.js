2ce9e4ba1bf5c74ec97655cccf99dbd848947f11
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-04-02 07:17:15 -0600

    Added hw-11

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,6 +1,6 @@
 // set the variables for later use
-Let rows, x_space;
-Let cols, y_space;
+let rows, x_space;
+let cols, y_space;
 
 function setup() {
     // create a canvas to draw my animation
@@ -21,18 +21,17 @@ function draw() {
     noStroke();
 
     //set a dummy value for x and y
-    Let x = 1;
-    Let y = 1;
+    let x = 1;
+    let y = 1;
     //set a while loop so that triangles appear in random locations on the canvas
     while ( x != 0 ){
       x = floor(random() * rows);
       y = floor(random() * cols);
 
-        for (Let y_space > 40;) {
-    //set a for loop so that the triangles change sizes and colors
+          translate(x, y);
           fill( random(50,100), random(175,255), random(125,80));
           triangle(288, 18, 351, 360, 288, 360);
 
-        }
+        
     }
 }
