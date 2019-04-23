9960a397866e9f3f507f63b2f3ca6cb16b13a498
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-04-22 21:56:27 -0600

    Update sketch.js

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -1,54 +1,57 @@
 //create a variable for the triangles
-let tri;
+let tri = [];
 
 function setup() {
-  //create canvas
-  createCanvas(windowWidth, 800);
+    //create canvas
+    createCanvas(windowWidth, windowHeight);
 
-  //create a new triangle "object" with the class type "tri"
-  tri = new Tri(width / 2, height / 2, 50, 'green');
+    //create a new triangle "object" with the class type "tri"
+    for (let i = 0; i < 20; i++) {
+        tri.push(new Tri(random(width), random(height), 50, 'green'));
+    }
 }
 
 function draw() {
-  background(0);
+    background(0);
 
-  //call tri methods
-  tri.display
-  tri.spin
+    //call tri methods
+    for (let i = 0; i < tri.length; i++) {
+        tri[i].display();
+        tri[i].spin();
+
+    }
 }
 
 //TRIANGLE CLASS DEFINITION
 class Tri {
-  constructor(x, y, size, color) {
-    this.color = color;
-    this.pointOneX = 10;
-    this.pointOneY = y-10;
-    this.pointTwoX = x+10;
-    this.pointTwoY = y+10;
-    this.pointThreeX = x-10;
-    this.pointThreeY = y+10;
-    this.posX = x;
-    this.posY = y;
-  }
+    constructor(x, y, size, color) {
+        this.color = color;
+        this.posX = x;
+        this.posY = y;
+        this.pointOneX = 0;
+        this.pointOneY = -10;
+        this.pointTwoX = 10;
+        this.pointTwoY = 10;
+        this.pointThreeX = -10;
+        this.pointThreeY = 10;
+        this.angle = 0;
+        this.delta = random(0.1);
+    }
 
-  display() {
-    push();
-    //remove outer stroke
-    noStroke();
-    //set fill color
-    fill(this.color);
-    //set the position of the triangles
-    translate(this.posX, this.posY);
-    triangle(this.pointOneX, this.pointOneY, this.pointTwoX, this.pointTwoY, this.pointThreeX, this.pointThreeY);
-    pop();
-  }
+    display() {
+        push();
+        //remove outer stroke
+        noStroke();
+        //set fill color
+        fill(this.color);
+        //set the position of the triangles
+        translate(this.posX, this.posY);
+        rotate(this.angle);
+        triangle(this.pointOneX, this.pointOneY, this.pointTwoX, this.pointTwoY, this.pointThreeX, this.pointThreeY);
+        pop();
+    }
 
-  spin() {
-    push();
-    noStroke();
-    fill(this.color);
-    translate(this.posX, this.posY);
-    //set the rotated points to oposite sides to create the illusion of "spinning"
-    triangle(this.pointThreeX, this.pointThreeY, this.pointOneX, this.pointOneY, this.pointTwoX, this.pointTwoY);
-  }
+    spin() {
+        this.angle += this.delta;
+    }
 }
