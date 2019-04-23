c88e7cd1ea034b7cddf01f214362a5fba28c7663
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-04-22 21:50:48 -0600

    Update sketch.js

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,56 +1,66 @@
 let stars = [];
 
-function setup(){
-  createCanvas( windowWidth, 800 );
-  frameRate(2);
+function setup() {
+    createCanvas(windowWidth, 800);
+    frameRate(2);
 
-  //create and cause the stars to move randomly
-  for (var i = 0; i < 20; i++) {
+    //create and cause the stars to move randomly
+    for (var i = 0; i < 20; i++) {
 
-    //add stars as objects to the array
-    stars.push(
-      {
-        maxSize: random(4),
-        pos: {
-          x: random(width),
-          y: random(height)
-        }
-      } //end stars object
-    ); //end array push
-  } //end for loop
+        //add stars as objects to the array
+        stars.push({
+            maxSize: random(4),
+            pos: {
+                x: random(width),
+                y: random(height)
+            }
+        } //end stars object
+        ); //end array push
+    } //end for loop
 }
 
 function draw() {
-  //set the background to black
-  background(0, 0, 0, 10);
+    //set the background to black
+    background(0, 0, 0, 10);
+
+    for (let i = 0; i < stars.length; i++) {
+        let star = stars[i];
+        drawStars(star);
+
+    }
 
 }
 
 //set star locations in an array
-Let starArr = [
-  [0, -9],
-  [7, 9],
-  [-9, -2],
-  [9, -2],
-  [-7, 9]
+let starPoints = [
+    [0, -9],
+    [7, 9],
+    [-9, -2],
+    [9, -2],
+    [-7, 9]
 ];
-let idx = 0;
 
-function drawStars( stars, time ) {
-  translate( width/2, height/2 );
+function drawStars(star) {
 
-  Let idxPlus = (idx + 1) % starArr.length;
 
-  Let x1 = starArr[idx][0] * 10;
-  Let y1 = starArr[idx][1] * 10;
-  Let x2 = starArr[idxPlus][0] * 10;
-  Let y2 = starArr[idxPlus][1] * 10;
 
-  //draw the sides of the stars
-  strokeWeight(18);
-  stroke(random(255), random(255), random(255), 150);
-  line(x1, y1, x2, y2);
+    push();
+    translate(star.pos.x, star.pos.y);
 
-  //increment the idx
-  idx = (idx + 1) % starArr.length;
+    for (let idx = 0; idx < starPoints.length; idx++) {
+        let idxPlus = (idx + 1) % starPoints.length;
+        let x1 = starPoints[idx][0] * star.maxSize;
+        let y1 = starPoints[idx][1] * star.maxSize;
+        let x2 = starPoints[idxPlus][0] * star.maxSize;
+        let y2 = starPoints[idxPlus][1] * star.maxSize;
+
+        //draw the sides of the stars
+        strokeWeight(4);
+        stroke(random(255), random(255), random(255), 150);
+        line(x1, y1, x2, y2);
+
+    }
+
+
+    pop();
 }
