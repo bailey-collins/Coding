a9addb22aa77e91cb56ce7f269a435cbe14c3a61
bailey-collins <42844089+bailey-collins@users.noreply.github.com>
2019-04-29 11:32:10 -0600

    Update sketch.js

--- hw-13/sketch.js
+++ hw-13/sketch.js
@@ -24,6 +24,8 @@ function draw() {
   //set background as the forest picture
   background( img );
   imageMode( CENTER );
+  spr.position.x = mouseX;
+  spr.position.y = mouseY;
   image( fairySprite, mouseX, mouseY);
   for (let i = 0; i< cats.length; i++) {
     fairy[1].frame();
